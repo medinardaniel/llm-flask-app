5db841f8aa48cb631376fb2e34f8a4071e4c07cd
Daniel Medina <daniel.m@selia.co>
2023-12-07 15:14:22 -0500

    turned debug=false

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def chat():
     return jsonify({"reply": reply})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
